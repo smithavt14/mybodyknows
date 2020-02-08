aae7d7d26afd77cba86a34089d1ce2a025144e9e
alex smith <smithavt14@gmail.com>
2020-02-09 00:40:28 +0800

    create quiz functionality

--- pages/result/result.js
+++ pages/result/result.js
@@ -10,7 +10,47 @@ Page({
     })
   },
 
+  // ----- Canvas Functionality -----
+  createCanvas: function () {
+    let context = wx.createCanvasContext('first')
+    context.stroke()
+    context.draw()
+  },
+
+  create2DCanvas: function () {
+    const query = wx.createSelectorQuery()
+    query.select('#first')
+      .fields({ node: true, size: true })
+      .exec((res) => {
+        const canvas = res[0].node
+        const ctx = canvas.getContext('2d')
+        const dpr = wx.getSystemInfoSync().pixelRatio
+        canvas.width = res[0].width * dpr
+        canvas.height = res[0].height * dpr
+        const img = canvas.createImage()
+        ctx.scale(dpr, dpr)
+      })
+  },
+
+  saveCanvas: function () {
+    this.create2DCanvas()
+    wx.canvasToTempFilePath({
+      canvasId: 'first',
+      success(res) {
+        let temp = res.tempFilePath
+        wx.saveImageToPhotosAlbum({
+          filePath: temp,
+          success(res) { console.log(res) }
+        })
+      }
+    })
+  },
+
   // ----- Lifecycle Functions -----
+  onReady: function () {
+    // this.create2DCanvas()
+  },
+  
   onLoad: function (options) {
     this.getCurrentUser()
     
